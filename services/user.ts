843aa2d287862853070a8545999f9bed3e5ade38
Helena Matoso <helena.matoso@aluno.ifsp.edu.br>
2026-04-22 08:41:52 -0300

    iniciando crud

---
+++ services/user.ts
@@ -0,0 +1,6 @@
+import { Prisma } from "@prisma/client"
+import { prisma } from "../libs/prisma"
+
+export const createUser = async (name:string, email: string) => {
+    const user = await prisma.user.
+}
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
-
+import { createUser} from "../services/user"
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.post('/user', async (req, res) => {
+    const user =  await createUser(
+        'John Doe',
+        'johndoe@example.com'
+       )
+
+    res.json(user)
+})
